310579e2b003006ab2520251e9673b718d737a91
Vasu <129766426+VasuBhalani@users.noreply.github.com>
2025-01-21 11:48:59 +0530

    Event handler hooks lifstate toggle project

--- thapa/props/src/main.jsx
+++ thapa/props/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './components/App.jsx'
+// import App from './components/NetflixCard/App.jsx'
+import {Event} from './components/EventHandling/EventHandle.jsx';
+import { EventHandlerasProps } from './components/EventHandling/EhasProps.jsx';
+import { UseStateHook } from './components/hooks/useStaeHook.jsx';
+import { DerivedState } from './components/hooks/PracuseStae.jsx';
+import {LiftingState} from './LiftState.jsx';
+import { ToggleSwitch } from './projects/Toggleswitch/ToggleSwitch.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>    
-    <App/>
-  </StrictMode>,
+    {/* <EventHandlerasProps/> */}
+    {/* <DerivedState/>*/}
+    {/* <LiftingState/> */}
+    <ToggleSwitch/>
+  </StrictMode>
 )
